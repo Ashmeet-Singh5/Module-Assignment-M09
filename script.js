4ae7cf783296a13431d97518e94022d807e2ffc2
Ashmeet-Singh5 <122590891+Ashmeet-Singh5@users.noreply.github.com>
2024-02-28 21:51:05 -0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,10 @@
+function clean() {
+    document.querySelectorAll('.dot').forEach(e => e.remove());
+  }
+  document.getElementById("dotDiv").addEventListener("click", function(event) {
+  var dot = document.createElement("div");
+  dot.className = "dot";
+  dot.style.left = (event.pageX - 4) + "px";
+  dot.style.top = (event.pageY - 4) + "px";
+  document.body.appendChild(dot);
+  });
